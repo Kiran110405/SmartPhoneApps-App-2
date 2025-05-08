a9acbfdb421566e27be9b78ab56331e940a48966
Kiran110405 <147168552+Kiran110405@users.noreply.github.com>
2025-05-08 12:25:29 +0100

    adding to new Task page

--- App2/AddTaskScreen.js
+++ App2/AddTaskScreen.js
@@ -5,6 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function HomeScreen() {
   const [modalVisible, setModalVisible] = useState(false);
   const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
 
   const DealSubmit = () => {
     console.log("User input:", inputText);
@@ -28,7 +29,10 @@ export default function HomeScreen() {
               style={styles.Textinput}
               placeholder="Your New Task..."
               value={inputText}
-              onChangeText={setInputText}
+              onChangeText={(t) => setInputText(t)}
+              onSubmitEditing={() => {
+                setOutputText(inputText);
+              }}
             ></TextInput>
             <Button title="Add" onPress={DealSubmit} />
           </View>
@@ -50,5 +54,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: "rgba(0,0,0,0.5)",
+  },
+
+  modalView: {
+    margin: 30,
+    backgroundColor: "#ffff",
+    padding: 20,
+    borderRadius: 20,
+  },
+
+  text: {
+    margin: 10,
+    fontSize: 20,
+    textAlign: "center",
+  },
+
+  Textinput: {
+    borderColor: "black",
+    borderWidth: 2,
+    padding: 10,
+    width: 200,
+    margin: 10,
+    textAlign: "center",
   },
 });
