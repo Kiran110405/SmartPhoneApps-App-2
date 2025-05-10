27f464f4add50d16dc2e4d0201f38b797396a6f7
Kiran110405 <147168552+Kiran110405@users.noreply.github.com>
2025-05-11 00:22:40 +0100

    Switch

--- App2/Settings.js
+++ App2/Settings.js
@@ -1,58 +1,35 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  Modal,
-  SafeAreaView,
-  Button,
-  Switch,
-} from "react-native";
-import React, { useState } from "react";
+import { useState } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
 
-export default function HomeScreen() {
-  const [modalVisible, setModalVisible] = useState(false);
+export default function App() {
   const [isEnabled, setIsEnabled] = useState(false);
 
   return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <Button
-          title="Theme"
-          onPress={() => {
-            setModalVisible(true);
-          }}
-        ></Button>
-      </SafeAreaView>
-      <Modal visible={modalVisible} animationType="slide" transparent={true}>
-        <View
-          style={{
-            backgroundColor: "#ff7777cc",
-            flex: 1,
-            justifyContent: "center",
-          }}
-        >
-          <View
-            style={[
-              styles.container,
-              { backgroundColor: isEnabled ? "#131313" : "#fff" },
-            ]}
-          >
-            <Text style={{ color: isEnabled ? "#fff" : "#000", fontSize: 24 }}>
-              Switch demo
-            </Text>
-            <Switch
-              trackColor={{ false: "gray", true: "#007AFF" }}
-              thumbColor={isEnabled ? "white" : "#007AFF"}
-              onValueChange={() => setIsEnabled((bool) => !bool)}
-              value={isEnabled}
-              style={{ margin: 15 }}
-            ></Switch>
-          </View>
-          <Button title="Done" onPress={() => setModalVisible(false)}></Button>
-        </View>
-      </Modal>
-    </>
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: isEnabled ? "#131313" : "#fff" },
+      ]}
+    >
+      <Text style={{ color: isEnabled ? "#fff" : "#000", fontSize: 24 }}>
+        Switch demo
+      </Text>
+      <Switch
+        trackColor={{ false: "gray", true: "#007AFF" }}
+        thumbColor={isEnabled ? "white" : "#007AFF"}
+        onValueChange={() => setIsEnabled((bool) => !bool)}
+        value={isEnabled}
+        style={{ margin: 15 }}
+      ></Switch>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
